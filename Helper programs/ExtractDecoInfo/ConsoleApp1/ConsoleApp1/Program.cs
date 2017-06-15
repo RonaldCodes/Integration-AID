bb009b5bcb906e48064131d51a6ba4b506b7aeac
Yaseenh <yaseenh@trackmatic.co.za>
2017-06-15 10:54:41 +0200

    Extract Deco info addded

---
+++ Helper programs/ExtractDecoInfo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Trackmatic.Rest.Batch.Requests;
+using Trackmatic.Rest.Core;
+using Trackmatic.Rest.Core.Requests;
+
+namespace ExtractDitAddresses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "KolokTest.csv"; //Write to a csv 
+            var api = Login("9408065009082", "yase191!", "224");
+            var take = 500;
+            var skip2 = 1;
+            var NumberOfPages = GetNumberOfPages(api, take, skip2);
+
+            for (int i = 0; i <= NumberOfPages; i++)
+            {
+                var percentage = Math.Round(i * 100 / NumberOfPages);
+                var skip = take * i;
+
+                Console.WriteLine($"{percentage}% Completed...");
+                WriteToFile(api, fileName, take, skip);
+            }
+
+        }
+
+        private static void WriteToFile(Api api, string fileName, int take, int skip)
+        {
+            var entities = api.ExecuteRequest(new LoadEntities(api.Context,null,take,skip)).Data.Data;
+
+            foreach (var entity in entities)
+            {
+                var id = entity.Id;
+                var entityReference = entity.Reference;
+                var entityName = entity.Name.ToString();
+
+                var decos = entity.Decos;
+                var decoId = decos.First().Id;
+
+                if (decoId == null)
+                {
+                    Console.WriteLine("Null found");
+                }
+                else if (decoId != null)
+                {
+                    var location = api.ExecuteRequest(new LoadLocation(api.Context, decos.First().DecoId)).Data;
+
+                    var decoName = location.Name;
+                    var decoShape = location.Shape.ToString();
+                    var lon = location.Location.Longitude;
+                    var lat = location.Location.Latitude;
+
+                    var contents = $"{entityReference},{entityName},{decoName},{decoShape},{lon},{lat}{Environment.NewLine}";
+                    var path = @"C:\Users\YaseenH\Desktop\Kolok2.csv";
+                    File.AppendAllText(path, contents);
+                }
+            }
+        }
+
+        private static double GetNumberOfPages(Api api, int take, int skip)
+        {
+            return Math.Ceiling(((api.ExecuteRequest(new LoadEntities(api.Context, null, take, skip))).Data.Total) / take);
+        }
+        private static Api Login(string user, string password, string clientId)
+        {
+            var api = new Api("https://rest.trackmatic.co.za/api/v1", clientId, user);
+            api.Authenticate(password);
+            return api;
+        }
+    }
+}
--- Helper programs/UploadPersonnelToAPI/ConsoleApp1/ConsoleApp1/Program.cs
+++ Helper programs/UploadPersonnelToAPI/ConsoleApp1/ConsoleApp1/Program.cs
@@ -22,7 +22,7 @@ namespace UploadPersonnelToApi
             api.Authenticate("yase191!");
 
 
-            string fname = @"C:\Users\YaseenH\Desktop\Integration\Integrations\AVI\Personnel3.xlsx"; //Read from xls
+            string fname = @"Files\PersonnelTemplate.xlsx"; //Read from xls
             Console.WriteLine("Reading file " + fname + ".");
 
             IWorkbook workBook = WorkbookFactory.Create(new FileStream(Path.GetFullPath(fname), FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
@@ -31,74 +31,88 @@ namespace UploadPersonnelToApi
             {
                 ISheet sheet = workBook.GetSheetAt(i);
 
-                for (int row = 1; row <= sheet.LastRowNum; row++)
+                for (int row = 4; row <= sheet.LastRowNum; row++)
                 {
                     ////PER Row
-                    var Id = "366/"+Guid.NewGuid();//(sheet.GetRow(row).GetCell(3)).ToString();
-                    var ClientId = "366";//(sheet.GetRow(row).GetCell(1)).ToString();
-                    var FirstName = (sheet.GetRow(row).GetCell(1, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
-                    var Surname = (sheet.GetRow(row).GetCell(2, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
-                    //var Gender =(sheet.GetRow(row).GetCell(4, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString().ToUpper();
-                    //var gender = Trackmatic.Rest.Core.Model.EGender.Unknown;
-                    //if (Gender == "MALE")
-                    //{
-                    //    gender = Trackmatic.Rest.Core.Model.EGender.Male;
-                    //}
-                    //if (Gender == "FEMALE")
-                    //{
-                    //    gender = Trackmatic.Rest.Core.Model.EGender.Female;
-                    //}
-                    //if (Gender == "Unknown")
-                    //{
-                    //    gender = Trackmatic.Rest.Core.Model.EGender.Unknown;
-                    //}
-                    var IdNo = (sheet.GetRow(row).GetCell(0, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
-                    var Mobile = ((sheet.GetRow(row).GetCell(9, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString());
-                    var primaryNum = (Mobile.Trim(new Char[] { ' ', '\'' })).Replace(" ", "");
-            //var Number = (sheet.GetRow(row).GetCell(7, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
-            //var Nature = (sheet.GetRow(row).GetCell(8, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString().ToUpper();
-            //var nature = Trackmatic.Rest.Core.Model.EPersonnelNature.Casual;
-            //if (Nature == "EMPLOYEE")
-            //{
-            //    nature = Trackmatic.Rest.Core.Model.EPersonnelNature.Employee;
-            //}
-            //if (Nature == "OWNER DRIVER")
-            //{
-            //    nature = Trackmatic.Rest.Core.Model.EPersonnelNature.OwnerDriver;
-            //}
-            //if (Nature == "THIRD PARTY")
-            //{
-            //    nature = Trackmatic.Rest.Core.Model.EPersonnelNature.ThirdParty;
-            //}
-            //if (Nature == "CASUAL")
-            //{
-            //    nature = Trackmatic.Rest.Core.Model.EPersonnelNature.Casual;
-            //}
+                    var Id = "355/" + Guid.NewGuid();//(sheet.GetRow(row).GetCell(3)).ToString();
+                    var ClientId = "355";//(sheet.GetRow(row).GetCell(1)).ToString();
 
-            //var Status = (sheet.GetRow(row).GetCell(9, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString().ToUpper();
-            //var status = Trackmatic.Rest.Core.Model.EPersonnelStatus.Inactive;
-            //if (Status == "ACTIVE")
-            //{
-            //    status = Trackmatic.Rest.Core.Model.EPersonnelStatus.Active;
-            //}
-            //if (Status == "CASUAL")
-            //{
-            //    status = Trackmatic.Rest.Core.Model.EPersonnelStatus.Inactive;
-            //}
 
-            //var Type = (sheet.GetRow(row).GetCell(10, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
-            //var type = Trackmatic.Rest.Core.Model.EPersonnelType.Driver;
-            //if (Type == "DRIVER")
-            //{
-            //    type = Trackmatic.Rest.Core.Model.EPersonnelType.Driver;
-            //}
-            //if (Type == "CREW")
-            //{
-            //    type = Trackmatic.Rest.Core.Model.EPersonnelType.Crew;
-            //}
-            var InternalRef = (sheet.GetRow(row).GetCell(3, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
-                    //var Default = (sheet.GetRow(row).GetCell(12, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+                    var firstName = (sheet.GetRow(row).GetCell(1, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+
+                    var surname = (sheet.GetRow(row).GetCell(2, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+
+                    var idNo = (sheet.GetRow(row).GetCell(3, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+                   // var internalReference = (sheet.GetRow(row).GetCell(4, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+
+                    var mobilePrimaryNumber = ((sheet.GetRow(row).GetCell(5, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString());
+                    var primaryContactNumber = (mobilePrimaryNumber.Trim(new Char[] { ' ', '\'' })).Replace(" ", "");
+
+                    //var secondaryContactNumber =(sheet.GetRow(row).GetCell(7, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+                    //var Status = (sheet.GetRow(row).GetCell(9, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString().ToUpper();
+                    //var status = Trackmatic.Rest.Core.Model.EPersonnelStatus.Inactive;
+                    //if (Status == "ACTIVE")
+                    //{
+                    //    status = Trackmatic.Rest.Core.Model.EPersonnelStatus.Active;
+                    //}
+                    //if (Status == "CASUAL")
+                    //{
+                    //    status = Trackmatic.Rest.Core.Model.EPersonnelStatus.Inactive;
+                    //}
+
+                    var Gender = (sheet.GetRow(row).GetCell(8, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString().ToUpper();
+                    var gender = Trackmatic.Rest.Core.Model.EGender.Unknown;
+                    if (Gender == "MALE")
+                    {
+                        gender = Trackmatic.Rest.Core.Model.EGender.Male;
+                    }
+                    if (Gender == "FEMALE")
+                    {
+                        gender = Trackmatic.Rest.Core.Model.EGender.Female;
+                    }
+                    if (Gender == "Unknown")
+                    {
+                        gender = Trackmatic.Rest.Core.Model.EGender.Unknown;
+                    }
+
+                    //var PersonnelTypes = (sheet.GetRow(row).GetCell(10, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+                    //var personnelType = Trackmatic.Rest.Core.Model.EPersonnelType.Driver;
+                    //if (PersonnelTypes == "DRIVER")
+                    //{
+                    //    personnelType = Trackmatic.Rest.Core.Model.EPersonnelType.Driver;
+                    //}
+                    //if (PersonnelTypes == "CREW")
+                    //{
+                    //    personnelType = Trackmatic.Rest.Core.Model.EPersonnelType.Crew;
+                    //}
+
+
+                    //var Nature = (sheet.GetRow(row).GetCell(8, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString().ToUpper();
+                    //var nature = Trackmatic.Rest.Core.Model.EPersonnelNature.Casual;
+                    //if (Nature == "EMPLOYEE")
+                    //{
+                    //    nature = Trackmatic.Rest.Core.Model.EPersonnelNature.Employee;
+                    //}
+                    //if (Nature == "OWNER DRIVER")
+                    //{
+                    //    nature = Trackmatic.Rest.Core.Model.EPersonnelNature.OwnerDriver;
+                    //}
+                    //if (Nature == "THIRD PARTY")
+                    //{
+                    //    nature = Trackmatic.Rest.Core.Model.EPersonnelNature.ThirdParty;
+                    //}
+                    //if (Nature == "CASUAL")
+                    //{
+                    //    nature = Trackmatic.Rest.Core.Model.EPersonnelNature.Casual;
+                    //}
+
                     //var Nationality = (sheet.GetRow(row).GetCell(13, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+
+
+
+
+                    //var Default = (sheet.GetRow(row).GetCell(12, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+
                     //var Tag = (sheet.GetRow(row).GetCell(14, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
                     //var CreatedOn = (sheet.GetRow(row).GetCell(15, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
                     //var createdOn = new DateTime();
@@ -114,7 +128,7 @@ namespace UploadPersonnelToApi
                     //    licenseExpiryDate = DateTime.Parse(LicenseExpiryDate);
                     //}
                     //var LicenseId = (sheet.GetRow(row).GetCell(18, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
-       
+
                     //var LicenseType = (sheet.GetRow(row).GetCell(19, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString().ToUpper();
                     //var licenseType = Trackmatic.Rest.Core.Model.ELicenseType.Unknown;
                     //if (LicenseType == "Unknown")
@@ -172,24 +186,26 @@ namespace UploadPersonnelToApi
 
                     //For blanks
                     //var someData = (sheet.GetRow(row).GetCell(1, MissingCellPolicy.CREATE_NULL_AS_BLANK)).ToString();
+
                     var p = new Personnel
                     {
-                  
-
                         Id = Id,
                         ClientId = ClientId,
-                        FirstName = FirstName,
-                        LastName = Surname,
-                        //Gender = gender,
-                        IdentityNumber = IdNo,
-                        PrimaryContactNo = primaryNum,
+
+                        FirstName = firstName,
+                        LastName = surname,
+                        IdentityNumber = idNo,
+                        //ReferenceNo = internalReference,
+                        PrimaryContactNo = primaryContactNumber,
                         //SecondaryContactNo = Number,
-                        //Nature = nature,
                         //Status = status,
+                        Gender = gender,
                         //Type = type,
-                        ReferenceNo = InternalRef,
-                        //DefaultVehicleRegistrationNumber = Default,
+                        //Nature = nature,
                         //Nationality = Nationality,
+ 
+                        //DefaultVehicleRegistrationNumber = Default,
+                        
                         //Tag = Tag,
                         //CreatedOn = createdOn,
                         //NickName = NickName,
@@ -198,7 +214,6 @@ namespace UploadPersonnelToApi
                         //LicenseType = licenseType,
 
                     };
-
              
                     api.ExecuteRequest(new SavePersonnel(api.Context, p));
                 }
