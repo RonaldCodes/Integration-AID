d5e919e66753973d3e3512ddbd0d9d0a75965b9b
Yaseenh <yaseenh@trackmatic.co.za>
2017-08-02 08:56:21 +0200

    Added excel consolidater program

---
+++ Helper programs/ExcelConsolidator/ExcelConsolidator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace ExcelConsolidator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var allCsv = Directory.EnumerateFiles(@"FilesToConsolidate", "*.csv", SearchOption.TopDirectoryOnly);
+            string[] header = { File.ReadLines(allCsv.First()).First(l => !string.IsNullOrWhiteSpace(l)) };
+            var mergedData = allCsv
+                .SelectMany(csv => File.ReadLines(csv)
+                    .SkipWhile(l => string.IsNullOrWhiteSpace(l)).Skip(1)); // skip header of each file
+            // Writes to bin folder
+            File.WriteAllLines(@"File.csv", header.Concat(mergedData));
+        }
+    }
+}
