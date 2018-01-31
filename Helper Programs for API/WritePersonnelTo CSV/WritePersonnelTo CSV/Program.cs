0e8425524f9d8fc0c314f42569dfc6f6b926b39c
Yaseenh <yaseenh@trackmatic.co.za>
2018-01-31 13:34:37 +0200

    Added personnel and vehicle

---
+++ Helper Programs for API/WritePersonnelTo CSV/WritePersonnelTo CSV/Program.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trackmatic.Rest.Batch;
+using Trackmatic.Rest.Batch.Queries;
+using Trackmatic.Rest.Core;
+using Trackmatic.Rest.Core.Model;
+using Trackmatic.Rest.Routing.Requests;
+
+namespace WritePersonnelTo_CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = Login("9408065009082", "yase191!", "252");
+
+            GetPersonnelInfo(api);
+        }
+
+        public static void GetPersonnelInfo(Api api)
+        {
+            try
+            {
+                var header = $"FirstName, FullName, Nature, IdentityNumber{Environment.NewLine}";
+                var path = $@"C:\Users\YaseenH\Desktop\Adhoc\personnel.csv";
+                File.AppendAllText(path, header);
+
+                var batch = new BatchQuery<Personnel>(new BatchOptions
+                {
+                    Write = 512,
+                    Read = 512
+                }, api, new LoadPersonnelInBatches(), PersonnelLookup);
+                batch.Execute();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.ToString()}...");
+            }
+        }
+
+        public static void PersonnelLookup(Api api, Personnel personnel)
+        {
+            var path = $@"C:\Users\YaseenH\Desktop\Adhoc\personnel.csv";
+            var contents = $"{personnel.FirstName}, {personnel.FullName},{personnel.Nature},{personnel.IdentityNumber}{Environment.NewLine}";
+            File.AppendAllText(path, contents);
+        }
+
+        private static Api Login(string user, string password, string clientId)
+        {
+            var api = new Api("https://rest.trackmatic.co.za/api/v1", clientId, user);
+            api.Authenticate(password);
+            return api;
+        }
+    }
+}
---
+++ Helper Programs for API/WriteVehicleToCSV/WriteVehicleToCSV/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trackmatic.Rest.Core;
+using Trackmatic.Rest.Core.Model;
+using Trackmatic.Rest.Core.Requests;
+
+namespace WriteVehicleToCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = Login("9408065009082", "yase191!", "252");
+
+            GetVehicleInfo(api);
+        }
+
+        public static void GetVehicleInfo(Api api)
+        {
+            try
+            {
+                var vehicles = api.ExecuteRequest(new LoadVehicles(api.Context)).Data;
+                WriteToTextFile(vehicles);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.ToString()}...");
+            }
+        }
+
+        public static void WriteToTextFile(List<VehicleInfo> vehicles)
+        {
+
+            var header = $"Registration, FleetNumber{Environment.NewLine}";
+            var path = $@"C:\Users\YaseenH\Desktop\Adhoc\Vehicles.csv";
+            File.AppendAllText(path, header);
+
+            foreach (var vehicle in vehicles)
+            {
+                var contents = $"{vehicle.RegistrationNumber}, {vehicle.CurrentCallSign}{Environment.NewLine}";
+                File.AppendAllText(path, contents);
+            }
+        }
+
+        private static Api Login(string user, string password, string clientId)
+        {
+            var api = new Api("https://rest.trackmatic.co.za/api/v1", clientId, user);
+            api.Authenticate(password);
+            return api;
+        }
+    }
+}
